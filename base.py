2a77635db2db4968ecc4f3aa19bbe52df9a253d7
mingtiancai <2427373908@qq.com>
2023-11-16 22:59:54 +0800

    移除验证函数

--- base.py
+++ base.py
@@ -2,9 +2,5 @@ from abc import ABCMeta, abstractmethod
 
 class base_sicp_class(metaclass=ABCMeta):
     @abstractmethod
-    def valid_func(self):
-        pass
-
-    @abstractmethod
     def test_func(self):
         pass
--- chapter2.py
+++ chapter2.py
@@ -1,11 +1,8 @@
 from base import base_sicp_class as bsc
 
 class t1(bsc):
-    def valid_func(self):
-        print("valid func")
 
     def test_func(self):
-        self.valid_func()
         print("test func")
 
 def test():
